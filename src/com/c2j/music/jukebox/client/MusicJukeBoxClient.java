62c443219bd2d43dc954221f3b7dd1adb3a066ff
Jun Park <jhpp114node@gmail.com>
2021-04-25 18:39:34 -0700

    created main mehtod with a simple print line

---
+++ src/com/c2j/music/jukebox/client/MusicJukeBoxClient.java
@@ -0,0 +1,7 @@
+package com.c2j.music.jukebox.client;
+
+public class MusicJukeBoxClient {
+    public static void main(String[] args) {
+        System.out.println("Music Jukebox Client");
+    }
+}
